945d619874b15692eed40dd7a3bea00bb6d9c245
Reto Gmür <reto@apache.org>
2014-08-31 21:42:05 +0200

    Issue #3: Invoking transformer for transforming containers

--- src/main/java/eu/fusepool/p3/proxy/ProxyHandler.java
+++ src/main/java/eu/fusepool/p3/proxy/ProxyHandler.java
@@ -15,18 +15,34 @@
  */
 package eu.fusepool.p3.proxy;
 
+import eu.fusepool.p3.vocab.ELDP;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.clerezza.rdf.core.Graph;
+import org.apache.clerezza.rdf.core.Resource;
+import org.apache.clerezza.rdf.core.Triple;
+import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.clerezza.rdf.core.serializedform.Parser;
+import org.apache.clerezza.rdf.core.serializedform.Serializer;
+import org.apache.clerezza.rdf.core.serializedform.SupportedFormat;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.TeeOutputStream;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpRequest;
@@ -35,18 +51,26 @@ import org.apache.http.ProtocolException;
 import org.apache.http.client.RedirectStrategy;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProxyHandler extends AbstractHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ProxyHandler.class);
     private final String targetBaseUri;
     private final CloseableHttpClient httpclient;
+    private final Parser parser = Parser.getInstance();
+    private final Serializer serializer = Serializer.getInstance();
 
     ProxyHandler(String targetBaseUri) {
         if (targetBaseUri.endsWith("/")) {
@@ -63,21 +87,27 @@ public class ProxyHandler extends AbstractHandler {
     public void handle(String target, Request baseRequest,
             final HttpServletRequest inRequest, final HttpServletResponse outResponse)
             throws IOException, ServletException {
-        final String targetUrlString = targetBaseUri + inRequest.getRequestURI();
-        //System.out.println(targetUrlString);
-        final URL targetUrl = new URL(targetUrlString);
+        final String targetUriString = targetBaseUri + inRequest.getRequestURI();
+        //System.out.println(targetUriString);
+        final URI targetUri;
+        try {
+            targetUri = new URI(targetUriString);
+        } catch (URISyntaxException ex) {
+            throw new IOException(ex);
+        }
+        final String method = inRequest.getMethod();
         final HttpEntityEnclosingRequestBase outRequest = new HttpEntityEnclosingRequestBase() {
 
             @Override
             public String getMethod() {
-                return inRequest.getMethod();
+                return method;
             }
 
         };
-        try {
-            outRequest.setURI(targetUrl.toURI());
-        } catch (URISyntaxException ex) {
-            throw new IOException(ex);
+        outRequest.setURI(targetUri);
+        String transformerUri = null;
+        if (method.equals("POST")) {
+            transformerUri = getTransformerUrl(getFullRequestUrl(inRequest), targetUri);
         }
         final Enumeration<String> headerNames = baseRequest.getHeaderNames();
         while (headerNames.hasMoreElements()) {
@@ -111,7 +141,7 @@ public class ProxyHandler extends AbstractHandler {
             if (entity != null) {
                 //outResponse.setContentType(target);
                 final InputStream instream = entity.getContent();
-                try {                    
+                try {
                     IOUtils.copy(instream, os);
                 } finally {
                     instream.close();
@@ -121,6 +151,118 @@ public class ProxyHandler extends AbstractHandler {
             os.flush();
         } finally {
             inResponse.close();
+        }
+        if (transformerUri != null) {
+            Header locationHeader = inResponse.getFirstHeader("Location");
+            if (locationHeader == null) {
+                log.warn("Response to POST request to LDPC contains no Location header. URI: " + targetUriString);
+            } else {
+                startTransformation(locationHeader.getValue(), targetUriString, transformerUri, inEntityBytes);
+            }
+        }
+    }
+
+    /*
+     * Rather than just checking if its RDF we should check the Liunke header following LDP Spec 5.2.1
+     */
+    private boolean isRdf(String contentTypeHeader) {
+        for (String supported : parser.getSupportedFormats()) {
+            if (supported.equalsIgnoreCase(contentTypeHeader)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String getFullRequestUrl(HttpServletRequest request) {
+        StringBuffer requestURL = request.getRequestURL();
+        if (request.getQueryString() != null) {
+            requestURL.append("?").append(request.getQueryString());
+        }
+        return requestURL.toString();
+    }
+
+    /**
+     * If targetUrl is a transforming container returns the URI of the
+     * associated container, otherwise null
+     */
+    private String getTransformerUrl(String requestedUri, URI targetUri) throws IOException {
+        HttpGet httpGet = new HttpGet(targetUri);
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            final Header contentTypeHeader = response.getFirstHeader("Content-Type");
+            if (contentTypeHeader == null) {
+                return null;
+            }
+            final String contentType = contentTypeHeader.getValue();
+            if (!isRdf(contentType)) {
+                return null;
+            }
+            HttpEntity entity = response.getEntity();
+            final UriRef baseUri = new UriRef(requestedUri);
+            final Graph graph = parser.parse(entity.getContent(), contentType, baseUri);
+            EntityUtils.consume(entity);
+            final Iterator<Triple> triples = graph.filter(baseUri, ELDP.transformer, null);
+            while (triples.hasNext()) {
+                Resource object = triples.next().getObject();
+                if (object instanceof UriRef) {
+                    return ((UriRef) object).getUnicodeString();
+                }
+            }
+        } finally {
+            response.close();
+        }
+        return null;
+    }
+
+    private void startTransformation(final String resourceUri, 
+            final String ldpcUri,
+            final String transformerUri,
+            final byte[] bytes) {
+        (new Thread() {
+
+            @Override
+            public void run() {
+                HttpPost httpPost = new HttpPost(transformerUri);
+                httpPost.setHeader("Content-Location", resourceUri);
+                httpPost.setEntity(new ByteArrayEntity(bytes));
+                try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+                    final HttpEntity entity = response.getEntity();
+                    final Header contentTypeHeader = entity.getContentType();
+                    final String contentType = contentTypeHeader.getValue();
+                    if (isRdf(contentType)) {
+                        Graph transformationResult = parser.parse(entity.getContent(), contentType);
+                        final ByteArrayOutputStream baos = new ByteArrayOutputStream((int) (entity.getContentLength()+3000));
+                        serializer.serialize(baos, transformationResult, SupportedFormat.TURTLE);
+                        final byte[] bytes = baos.toByteArray();
+                        final StringWriter turtleString = new StringWriter(baos.size()+2000);
+                        turtleString.append(new String(bytes, "UTF-8"));
+                        turtleString.append('\n');
+                        turtleString.append("<> "+ELDP.transformedFrom+" <"+resourceUri+ "> .");
+                        post(ldpcUri, new ByteArrayEntity(turtleString.toString().getBytes("UTF-8")), "text/turtle", resourceUri);
+                    } else {
+                        post(ldpcUri, entity, contentType, resourceUri);
+                    }
+                    EntityUtils.consume(entity);
+                } catch (IOException ex) {
+                    log.error("Error executing transformer: "+transformerUri, ex);
+                }
+            }
+
+        }).start();
+    }
+    
+    private void post(String ldpcUri, HttpEntity entity, String mediaType,
+            final String extractedFromUri) throws IOException {
+        HttpPost httpPost = new HttpPost(ldpcUri);
+        httpPost.setHeader("Slug", extractedFromUri.substring(extractedFromUri.lastIndexOf('/'))+"-transformed");
+        httpPost.setHeader("Content-type", mediaType);
+        httpPost.setEntity(entity);
+        try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+            if (response.getStatusLine().getStatusCode() != 201) {
+                log.warn("Response to POST request to LDPC resulted in: "
+                        +response.getStatusLine()+" rather than 201. URI: "+ldpcUri);
+            }
         }
     }
 
--- src/test/java/eu/fusepool/p3/proxy/ProxyTest.java
+++ src/test/java/eu/fusepool/p3/proxy/ProxyTest.java
@@ -92,46 +92,59 @@ public class ProxyTest {
                 + "@prefix ldp: <http://www.w3.org/ns/ldp#>.\n"
                 + "@prefix eldp: <http://vocab.fusepool.info/eldp#>.\n"
                 + "\n"
-                + "<http://example.org/container1/>\n"
+                + "<http://localhost:"+proxyPort+"/container1/>\n"
                 + "   a ldp:DirectContainer;\n"
                 + "   dcterms:title \"An extracting LDP Container using simple-transformer\";\n"
-                + "   ldp:membershipResource <http://example.org/container1/>;\n"
+                + "   ldp:membershipResource <http://localhost:"+proxyPort+"/container1/>;\n"
                 + "   ldp:hasMemberRelation ldp:member;\n"
                 + "   ldp:insertedContentRelation ldp:MemberSubject;\n"
-                + "   fp:transformer <http://localhost"+backendPort+"/simple-transformer>.";
-        stubFor(get(urlEqualTo("/my/resource"))
-                .withHeader("Accept", equalTo("text/turtle"))
+                + "   eldp:transformer <http://localhost:"+backendPort+"/simple-transformer>.";
+        stubFor(get(urlEqualTo("/container1/"))
+                //.withHeader("Accept", equalTo("text/turtle"))
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.SC_OK)
                         .withHeader("Content-Type", "text/turtle")
                         .withBody(turtleLdpc)));
         
-        stubFor(post(urlEqualTo("/my/resource"))
+        stubFor(post(urlEqualTo("/container1/"))
                 //.withHeader("Conetnt-Type", matching("text/plain*"))
                 .willReturn(aResponse()
-                        .withStatus(HttpStatus.SC_CREATED)));
+                        .withStatus(HttpStatus.SC_CREATED)
+                        .withHeader("Location", "http://localhost:"+proxyPort+"/container1/new-resource")));
         
         //A GET request returns the unmodified answer
         RestAssured.given().header("Accept", "text/turtle")
                 .expect().statusCode(HttpStatus.SC_OK).header("Content-Type", "text/turtle").when()
-                .get("/my/resource");
+                .get("/container1/");
         //Certainly the backend got the request
-        verify(getRequestedFor(urlMatching("/my/resource")));
+        verify(getRequestedFor(urlMatching("/container1/")));
         
         //Let's post some content
         RestAssured.given()
                 .contentType("text/plain;charset=UTF-8")
                 .content("hello")
                 .expect().statusCode(HttpStatus.SC_CREATED).when()
-                .post("/my/resource");
+                .post("/container1/");
         //the backend got the post request aiaginst the LDPC
-        verify(postRequestedFor(urlMatching("/my/resource")));
+        verify(postRequestedFor(urlMatching("/container1/")));
         //and after a while also against the Transformer
-        Thread.sleep(100);
-        //TODO make this test pass (Issue #3)
-        //verify(getRequestedFor(urlMatching("/simple-transformer")));
-        //Since the media-type is supported
-        //verify(postRequestedFor(urlMatching("/simple-transformer")));
+        //first the transformer whould be checcked if the format matches
+        //wait and try: verify(getRequestedFor(urlMatching("/simple-transformer")));
+        //then we will get a POST (since media type is supported)
+        int i = 0;
+        while (true) {
+            Thread.sleep(100);
+            try {
+                verify(postRequestedFor(urlMatching("/simple-transformer")));
+                break;
+            } catch (Error e) {
+                
+            }
+            if (i++ > 600) {
+                //after one minute for real:
+                verify(postRequestedFor(urlMatching("/simple-transformer")));
+            }
+        }
     }
 
     public static int findFreePort() {
